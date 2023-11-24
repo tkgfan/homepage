f83ea93747de81306911173454378b5103858876
霒蚀 <1029129827@qq.com>
2023-11-24 23:16:32 +0800

    feat: web内容

--- web/config.js
+++ web/config.js
@@ -1,3 +1,29 @@
 const CONFIG = {
-    title: "acsupport"
+    header: {
+        icon: 'https://api.iconify.design/bxl:dev-to.svg',
+        title: "acsupport",
+        subTitle: '开发者中心',
+    },
+    links: [{
+        icon: 'https://api.iconify.design/simple-icons:gitee.svg',
+        title: 'Gitee',
+        url: 'https://e.gitee.com/acsupport/dashboard'
+    },
+    {
+        icon: 'https://api.iconify.design/simple-icons:nginx.svg',
+        title: 'Nginx',
+        url: 'http://test-gateway.gmfan.cn/'
+    }, {
+        icon: 'https://api.iconify.design/simple-icons:portainer.svg',
+        title: 'Portainer',
+        url: 'http://test-portainer.gmfan.cn/#!/home'
+    }, {
+        icon: 'https://api.iconify.design/simple-icons:github.svg',
+        title: 'Github',
+        url: 'https://github.com/tkgfan/homepage'
+    }, {
+        icon: 'https://api.iconify.design/simple-icons:apifox.svg',
+        title: 'Api Docs',
+        url: 'https://github.com/tkgfan/homepage'
+    }]
 }
